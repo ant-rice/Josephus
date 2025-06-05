598212a160facee215d0efe13ebba006a7402e2d
ant-rice <antonio_rice@icloud.com>
2025-06-05 15:26:21 -0700

    finished add and sim method started on slim

--- JosephusSim.java
+++ JosephusSim.java
@@ -11,8 +11,8 @@ public class JosephusSim {
       try {
          // load names from the file in order, generating a singly linked list of PersonNodes
          Scanner file = new Scanner(new File(fileName));
-         while(file.nextLine()) {
-            String name = file.hasNextLine();
+         while(file.hasNextLine()) {
+            String name = file.nextLine();
             
             add(name);
             size++;
@@ -22,7 +22,12 @@ public class JosephusSim {
          // remember the last node as the one in front of the next to get eliminated
          
          // generate, print, and save the random elimination count
+         
+         Random r = new Random();
+         eliminationCount = r.nextInt(size) + 1;
+         System.out.println("=== Elimination count is " + eliminationCount + " ===");
 
+         
       } catch(FileNotFoundException e) {
          System.out.println("Something went wrong with " + fileName);
       }
@@ -30,18 +35,27 @@ public class JosephusSim {
    
    // optional helper method for constructing the circle
    private void add(String val) {
-      PersonNode nextName = new PersonNode(val);
-      track = nextName;
-      circle = newName;
+      PersonNode newNode = new PersonNode(val);
+      if (circle == null) {
+         track = newNode;
+         circle = newNode;
+      } else {
+         track = newNode;
+         circle = newNode;
+      }
    }
    
    public void eliminate() {
       // count to the elimination count
-      
+      int eliminating;
+      for(int i = 0; i < eliminationCount - 1; i++) {
+         track = circle(i);
+      }
       // print who will be eliminated
-      
+      System.out.println(track + " eliminated!");
       // eliminate the person and update "front" of the circle and size
-
+      remove.track;
+      track = newNode;
    }
    
    public boolean isOver() {
